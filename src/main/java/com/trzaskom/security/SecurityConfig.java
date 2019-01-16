08ae909de1be704e4229d58968a1ffd2a0523eee
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 19:15:59 +0100

    Add security configuration

---
+++ src/main/java/com/trzaskom/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.trzaskom.security;
+
+import com.trzaskom.security.jwt.JWTConfigurer;
+import com.trzaskom.security.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by miki on 2019-01-04.
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+
+    public SecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager customauthenticationManagerBean() throws Exception {
+        return authenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+                    http
+                        .csrf()
+                            .disable()
+                        .cors()
+                            .and()
+                        .sessionManagement()
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                            .and()
+                //.httpBasic() // optional, if you want to access
+                //  .and()     // the services from a browser
+                        .authorizeRequests()
+                            .antMatchers("/signup").permitAll()
+                            .antMatchers("/login").permitAll()
+                            .antMatchers("/public").permitAll()
+                            .anyRequest().authenticated()
+                            .and()
+                        .apply(new JWTConfigurer(this.tokenProvider));
+                    // @formatter:on
+    }
+
+}
+
