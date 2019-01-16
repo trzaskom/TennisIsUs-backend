2706a985edd162f048743f9130d70f210e44247f
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 19:08:08 +0100

    Add new query metods

--- src/main/java/com/trzaskom/jpa/repository/UserRepository.java
+++ src/main/java/com/trzaskom/jpa/repository/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUsername(String username);
+    public boolean existsByUsername(String username);
 }
