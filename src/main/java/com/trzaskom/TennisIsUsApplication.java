8319e72b91d9989f836e5b97d11bc8d7ca07303b
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 19:09:40 +0100

    Add logger

--- src/main/java/com/trzaskom/TennisIsUsApplication.java
+++ src/main/java/com/trzaskom/TennisIsUsApplication.java
@@ -1,5 +1,7 @@
-package com.trzaskom.jpa;
+package com.trzaskom;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class TennisIsUsApplication {
+
+	public final static Logger logger = LoggerFactory.getLogger(TennisIsUsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TennisIsUsApplication.class, args);
