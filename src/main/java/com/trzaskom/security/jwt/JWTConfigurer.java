b93bdf0b271fbc4ef55690b1e04d2c93d9d1f839
trzaskom <m_trzaskowski@wp.pl>
2019-01-16 19:13:07 +0100

    Add JWT configuration for authentication

---
+++ src/main/java/com/trzaskom/security/jwt/JWTConfigurer.java
@@ -0,0 +1,24 @@
+package com.trzaskom.security.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+/**
+ * Created by miki on 2019-01-04.
+ */
+public class JWTConfigurer
+        extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    public JWTConfigurer(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JWTFilter customFilter = new JWTFilter(this.tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
