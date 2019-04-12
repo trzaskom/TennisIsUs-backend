18d98e31a7720a8bf1951ba7af855b1bd9c2bad3
trzaskom <m_trzaskowski@wp.pl>
2019-04-12 14:24:37 +0200

    Add query methods for finding users who fulfil search criteria

--- src/main/java/com/trzaskom/jpa/repository/GeolocationRepository.java
+++ src/main/java/com/trzaskom/jpa/repository/GeolocationRepository.java
@@ -2,7 +2,10 @@ package com.trzaskom.jpa.repository;
 
 import com.trzaskom.jpa.model.Geolocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by miki on 2019-01-19.
@@ -10,6 +13,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GeolocationRepository extends JpaRepository<Geolocation, Long> {
+
+    @Query(value = "SELECT geolocations.* FROM geolocations JOIN users ON (users.id = geolocations.user_id AND (users.rating BETWEEN ?1 AND ?2) AND (users.age BETWEEN ?3 AND ?4) AND users.id != ?5)", nativeQuery = true)
+    List<Geolocation> findByCryteriaBarringGender(Double lowestRating, Double highestRating, Integer youngest, Integer oldest, Long userId);
+
+    @Query(value = "SELECT geolocations.* FROM geolocations JOIN users ON (users.id = geolocations.user_id AND (users.rating BETWEEN ?1 AND ?2) AND (users.age BETWEEN ?3 AND ?4) AND users.id != ?5 AND users.gender = ?6)", nativeQuery = true)
+    List<Geolocation> findByCryteriaWithGender(Double lowestRating, Double highestRating, Integer youngest, Integer oldest, Long userId, Integer genderCode);
+
 }
 
 
